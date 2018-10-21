599486415c9fa9a9f18e23293d27da1a671ecd69
Mike de Groot <mike.j.degroot@gmail.com>
2018-10-21 13:23:36 +0100

    state being updated on click

--- src/components/home.js
+++ src/components/home.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {count : 0};
+  }
+
+
+  handleClick() {
+    this.setState(prevState => ({count : prevState.count + 1}));
+  }
+
   render() {
     return(
-      <h1> Hello </h1>
+      <div>
+        <h1> Hello </h1>
+        <button onClick={()=> {this.handleClick()}}> Click state {this.state.count} </button>
+      </div>
     )
   }
 }
